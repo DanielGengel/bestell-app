5311873b36b81225a12eeea3ac42a9fa615ad599
DanielGengel <daniel.gengel@outlook.com>
2026-05-15 18:57:34 +0200

    First commit

---
+++ scripts/menuDB.js
@@ -0,0 +1,89 @@
+ let menuDB = [
+    {
+      "category": "Burger & Sandwiches",
+      "items": [
+        {
+          "picture": "img",
+          "name": "Veggie mushroom black burger",
+          "description": "Pickl green salad, Tomatoes, Edamame, Mushroom",
+          "price": 16.90
+        },
+        {
+          "picture": "img",
+          "name": "All meat burger",
+          "description": "Beef, Bacon, Dill pickles, Smoked cheese, Ketchup, BBQ sauce",
+          "price": 15.90
+        },
+        {
+          "picture": "img",
+          "name": "Beef red burger",
+          "description": "Beef, Cheese, Tomatoes, Lettuce, Onion",
+          "price": 14.90
+        },
+        {
+          "picture": "img",
+          "name": "Big chicken burger",
+          "description": "Chicken, Cheese, Tomatoes, Lettuce, Onion, Bell pepper",
+          "price": 15.90
+        }
+      ]
+    },
+    {
+      "category": "Pizza (30cm)",
+      "items": [
+        {
+          "picture": "img",
+          "name": "Pizza Margherita",
+          "description": "Tomato Sauce, Mozzarella",
+          "price": 11.90
+        },
+        {
+          "picture": "img",
+          "name": "Pizza Chorizo",
+          "description": "Tomato Sauce, Mozzarella, Chorizo",
+          "price": 13.90
+        },
+        {
+          "picture": "img",
+          "name": "Funghi",
+          "description": "Pomodoro, Olives, Button Mushrooms, Mozzarella",
+          "price": 12.90
+        },
+        {
+          "picture": "img",
+          "name": "Quattro Formaggi with Chicken",
+          "description": "Chicken, Mozzarella, Gorgonzola, Fontina, Parmesan Reggiano",
+          "price": 15.90
+        }
+      ]
+    },
+    {
+      "category": "Salad",
+      "items": [
+        {
+          "picture": "img",
+          "name": "Warm beef arugula salad",
+          "description": "Beef, Arugula, Field salad, Green life, Cherry tomatoes, Sun dried tomatoes, Balsamic-vinegar dressing",
+          "price": 16.90
+        },
+        {
+          "picture": "img",
+          "name": "Mini green Salad",
+          "description": "Green salad, Cucumber, Carrots, Parsley, Tomatoes",
+          "price": 7.90
+        },
+        {
+          "picture": "img",
+          "name": "Green Salad with sea food",
+          "description": "Mixed greens, Cherry tomatoes, Red onion, Avocado, Squid rings, Shrimp, Dijon mustard-honey dressing with dill",
+          "price": 16.90
+        },
+        {
+          "picture": "img",
+          "name": "Vegan green salad with tofu",
+          "description": "Green salad, Cherry tomatoes, Cucumber, Baby spinach, Edamame, Radishes, Beetroot, Tofu, Peanuts",
+          "price": 14.90
+        }
+      ]
+    }
+  ]
