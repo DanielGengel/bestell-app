9bb30a1312d8e375b2d91f9c70aaea5f5c16b623
DanielGengel <daniel.gengel@outlook.com>
2026-05-19 22:13:17 +0200

    All buttons working and functions splitted and some renamed.

--- script.js
+++ script.js
@@ -1,140 +1,42 @@
 let foodMenu = JSON.parse(localStorage.getItem("foodMenu")) || menuDB;
 let customerBasket = JSON.parse(localStorage.getItem("customerBasket")) || [];
-// let customerSubtotal = JSON.parse(localStorage.getItem("customerSubtotal")) || [];
-// let subTotal = 0;
 const menuSection = document.getElementById("menuSectionID");
 
 function renderFoodMenu() {
     menuSection.innerHTML = "";
     let foodMenuHTML = "";
     for (let index = 0; index < foodMenu.length; index++) {
-        // console.log("---------> Category = " + foodMenu[index].category);
         foodMenuHTML = getCategoryTemplate(index);
 
         for (let index2 = 0; index2 < foodMenu[index].items.length; index2++) {
-            // console.log("./assets/img/" + foodMenu[index].items[index2].picture);
-            // console.log(foodMenu[index].items[index2].name);
-            // console.log(foodMenu[index].items[index2].description);
-            // console.log(foodMenu[index].items[index2].price);
             foodMenuHTML += getArticleTemplate(index, index2);
         }
         menuSection.innerHTML += foodMenuHTML;
     }
 
-    addEventsToButtons(".btnAddToCart", addToCart);
+    addButtonEvents(".btnAddToCart", addToCart);
 
     renderCart();
     restoreBadges(); // in case of browser refresh
 
+    // Switch between desktop and mobile cart view if window is resized
+    window.addEventListener("resize", renderCart);
 }
 
-function addEventsToButtons(buttonClass, functionToRun) {
-    const buttons = document.querySelectorAll(buttonClass);
+function addButtonEvents(buttonClass, action) {
+    let buttons = document.querySelectorAll(buttonClass);
+
     for (let i = 0; i < buttons.length; i++) {
-        buttons[i].addEventListener("click", function (event) {
-            event.stopPropagation();
-            const index = Number(buttons[i].dataset.index.trim());
-            functionToRun(index);
+        buttons[i].addEventListener("click", function () {
+            let articleID = Number(this.dataset.index);
+
+            action(articleID);
         });
     }
 }
-
-// function addToCart(productID) {
-//     const item = basketArray.find((item) => item.productID === productID);
-
-//     if (item) {
-//         item.quantity++;
-
-//     } else {
-//         basketArray.push({
-//             productID,
-//             quantity: 1,
-//         });
-//     }
-
-//     console.log(basketArray);
-// }
-
-// basketArray
-// let cart = [];
-
-function addToCart(articleID) {
-    // Find choosen article in menuDB
-    let foundItem = null;
-
-    for (let category of foodMenu) {
-        for (let item of category.items) {
-            if (item.articleID === articleID) {
-                foundItem = item;
-            }
-        }
-    }
-    const badge = document.getElementById(`badgeID-${foundItem.articleID}`);
-    // Check if article is already in Cart
-    let existingCartItem = null;
-
-    for (let cartItem of customerBasket) {
-        if (cartItem.articleID === articleID) {
-            existingCartItem = cartItem;
-        }
-    }
-
-    // Article is already in cart -> update amount and totalPrice
-    if (existingCartItem) {
-        existingCartItem.amount++;
-        existingCartItem.totalPrice = existingCartItem.amount * existingCartItem.singlePrice;
-        //  renderCart();
-        // scrollToCartItem(foundItem.articleID);
-        // console.log(foundItem.articleID);
-        badge.textContent = existingCartItem.amount;
-    }
-
-    // Article not in cart yet, add...
-    else {
-        customerBasket.push({
-            articleID: foundItem.articleID,
-            name: foundItem.name,
-            singlePrice: foundItem.price,
-            amount: 1,
-            totalPrice: foundItem.price,
-        });
-        badge.textContent = 1;
-        //  renderCart();
-        // scrollCartToBottom();
-    }
-    // Update local storage
-    localStorage.setItem("customerBasket", JSON.stringify(customerBasket));
-
-
-
-    badge.style.display = "flex";
-
-    renderCart();
-    scrollToCartItem(foundItem.articleID);
-    console.log(foundItem.articleID);
-}
-
-// // TODO: Save sumTotal to local storage
-// function updateCustomerReceipt() {
-//     for (let index = 0; index < customerBasket.length; index++) {
-//         console.log(customerBasket[index].totalPrice);
-//         subTotal += customerBasket[index].totalPrice;
-//     }
-// }
-
-// function scrollCartToBottom() {
-//     const cart = document.querySelector(".items");
-
-//     // TODO: if container is near bottom dont scroll
-//     // const isNearBottom =
-//     // cart.scrollTop + cart.clientHeight >=
-//     // cart.scrollHeight - 50;
-
-//     cart.scrollTop = cart.scrollHeight;
-// }
 
 function scrollToCartItem(itemId) {
-    // TODO: if container is near bottom dont scroll
+    // TODO: if container is near bottom don't scroll
     const element = document.getElementById(`itemID-${itemId}`);
     console.log("itemID-${itemId} => " + `itemID-${itemId}`);
 
@@ -165,7 +67,7 @@ function renderCart() {
         for (let index = 0; index < customerBasket.length; index++) {
             cartContentHTML += getShoppingCartTemplateMain(index);
         }
-const totals = calculateTotals();
+        const totals = calculateTotals();
         cartContentHTML += getShoppingCartTemplateFooter(totals);
     }
 
@@ -178,14 +80,12 @@ const totals = calculateTotals();
         desktopCart.append(content);
         mobileCart.close();
     }
+
+    // initCartButtons();
+    addButtonEvents(".btnAddOneArticle", (id) => changeArticleAmount(id, 1));
+    addButtonEvents(".btnRemoveOneArticle", (id) => changeArticleAmount(id, -1));
+    addButtonEvents(".btnRemoveAllArticles", (id) => deleteArticle(id));
 }
-
-// Switch between desktop and mobile cart view if window is resized
-window.addEventListener("resize", renderCart);
-
-// function updateLocalStorage() {
-//     localStorage.setItem("foodMenu", JSON.stringify(foodMenu));
-// }
 
 function restoreBadges() {
     for (let cartItem of customerBasket) {
@@ -201,22 +101,144 @@ function restoreBadges() {
     }
 }
 
-
-
 function calculateTotals() {
+    let subtotal = 0;
+    let delivery = 0;
+    
+    for (let i = 0; i < customerBasket.length; i++) {
+        subtotal = subtotal + customerBasket[i].amount * customerBasket[i].singlePrice;
+    }
 
-    const subtotal = customerBasket.reduce(
-        (sum, item) => sum + (item.amount * item.singlePrice),
-        0
-    );
+    // Free delivery for orders aboce 50€
+    if (subtotal < 50) {
+        delivery = 4.99;
+    }
 
-    const delivery = subtotal > 0 ? 4.99 : 0;
-
-    const total = subtotal + delivery;
+    let total = subtotal + delivery;
 
     return {
-        subtotal,
-        delivery,
-        total
+        subtotal: subtotal,
+        delivery: delivery,
+        total: total,
     };
+}
+
+function addToCart(articleID) {
+    // Find article in menuDB and return object
+    let article = findArticleByID(articleID);
+
+    // Doesn't exist...
+    if (!article) {
+        return;
+    }
+
+    // Check if article already in cart
+    let cartItem = findCartItem(articleID);
+    if (cartItem) {
+        changeArticleAmount(articleID, 1);
+    } else {
+        createNewCartItem(article);
+    }
+
+    updateBadge(articleID);
+    saveBasket();
+    renderCart();
+    scrollToCartItem(articleID);
+}
+
+function findArticleByID(articleID) {
+    for (let i = 0; i < foodMenu.length; i++) {
+        let category = foodMenu[i];
+
+        for (let j = 0; j < category.items.length; j++) {
+            let item = category.items[j];
+
+            if (item.articleID === articleID) {
+                return item;
+            }
+        }
+    }
+
+    return null;
+}
+
+// Check if article is already in cart
+function findCartItem(articleID) {
+    for (let i = 0; i < customerBasket.length; i++) {
+        let cartItem = customerBasket[i];
+
+        if (cartItem.articleID === articleID) {
+            return cartItem;
+        }
+    }
+
+    return null;
+}
+
+// If article not in cart already, add to card
+function createNewCartItem(article) {
+    customerBasket.push({
+        articleID: article.articleID,
+        name: article.name,
+        singlePrice: article.price,
+        amount: 1,
+        totalPrice: article.price,
+    });
+}
+
+// Delete article from basket
+function deleteArticle(articleID) {
+    for (let i = 0; i < customerBasket.length; i++) {
+        if (customerBasket[i].articleID === articleID) {
+            customerBasket.splice(i, 1);
+        }
+    }
+
+    updateBadge(articleID);
+    saveBasket();
+    renderCart();
+}
+
+function updateBadge(articleID) {
+    let badge = document.getElementById(`badgeID-${articleID}`);
+
+    let cartItem = findCartItem(articleID);
+
+    if (!badge) {
+        return;
+    }
+
+    if (cartItem) {
+        badge.style.display = "flex";
+        badge.textContent = cartItem.amount;
+    } else {
+        badge.style.display = "none";
+        badge.textContent = 0;
+    }
+}
+
+// Update locale storage
+function saveBasket() {
+    localStorage.setItem("customerBasket", JSON.stringify(customerBasket));
+}
+
+// Add or remove one item from specific article (articleID)
+// value = plus or minus 1
+function changeArticleAmount(articleID, value) {
+    let cartItem = findCartItem(articleID);
+
+    if (!cartItem) {
+        return;
+    }
+
+    cartItem.amount = cartItem.amount + value;
+    cartItem.totalPrice = cartItem.amount * cartItem.singlePrice;
+
+    if (cartItem.amount <= 0) {
+        deleteArticle(articleID);
+    }
+
+    updateBadge(articleID);
+    saveBasket();
+    renderCart();
 }
